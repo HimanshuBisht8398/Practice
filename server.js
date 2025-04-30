19596412094a42971107a3b816598ba3595f10d6
himanshubisht98 <himanshu.bisht@nivaancare.com>
2025-04-30 12:30:30 +0530

    1st change

---
+++ server.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const app=express()
+
+
+app.get('/',(req,res)=>{
+    res.send({code:200,message:"Hello Backend is runing successfully!!"})
+})
+app.listen(4000,()=>{
+    console.log('server running on port 4000')
+})
