2b972ebea638a5761997494a6cc40197b1be6b3c
himanshubisht98 <himanshu.bisht@nivaancare.com>
2025-04-30 12:46:13 +0530

    changes2

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express=require('express')
 const app=express()
 console.log("cherry pick======")
 
+function Add(){
+    return 8+1;
+}
+setTimeout(()=>{
+    console.log("hello")
+},2000)
 app.get('/',(req,res)=>{
     res.send({code:200,message:"Hello Backend is runing successfully!!"})
 })
